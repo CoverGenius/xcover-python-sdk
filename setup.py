e535dd6f25f4a9f8ad7823ff8139d4fab7a0d7ee
Wudit <udit@rentalcover.com>
2018-12-06 14:27:36 +1100

    Changed requirements in setup.py file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='xcovlib',
-    version='0.1.0',
+    version='0.5.0',
     description='XCover API client library for Python',
     long_description=readme,
     author='Udit Agarwal',
@@ -21,10 +21,6 @@ setup(
     url='https://github.com/CoverGenius/xcover-python-sdk/',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=[
-        'atomicwrites==1.2.1', 'attrs==18.2.0', 'httpsig==1.3.0',
-        'more-itertools==4.3.0', 'pluggy==0.8.0', 'py==1.7.0',
-        'pycryptodome==3.7.2', 'requests'
-    ],  # external packages as dependencies
+    install_requires=['pycryptodome==3.7.2', 'httpsig', 'requests'],  # external packages as dependencies
 )
 
